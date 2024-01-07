c3eddd47daa096beeb0c44b0e53bbbfb50aee39c
Oleksandr Makedon <rbkrutoi@gmail.com>
2024-01-07 04:22:49 +0100

    fix install bugs

--- utils/create_files.py
+++ utils/create_files.py
@@ -2,7 +2,7 @@ import os
 import json
 from typing import Union
 
-from data.config import IMPORTANT_FILES, DB
+from data.config import IMPORTANT_FILES, DB, DB_DIR
 
 
 def join_path(path: Union[str, tuple, list]) -> str:
@@ -16,24 +16,27 @@ def touch(path: Union[str, tuple, list], file: bool = False) -> bool:
     path = join_path(path)
     if file:
         if not os.path.exists(path):
+            # Создаем все промежуточные каталоги
+            os.makedirs(os.path.dirname(path), exist_ok=True)
             with open(path, 'w') as f:
                 if path == DB:
                     db = {}
                     f.write(json.dumps(db))
                 else:
-                    f.write('')  
-
+                    f.write('')
             return True
+        else:
+            return False
+    else:
+        if not os.path.isdir(path):
+            os.makedirs(path, exist_ok=True)
+            return True
+        else:
+            return False
 
-        return False
-
-    if not os.path.isdir(path):
-        os.mkdir(path)
-        return True
-
-    return False
 
 
 def create_files():
+    touch(DB_DIR)
     for file in IMPORTANT_FILES:
         touch(path=file, file=True)
